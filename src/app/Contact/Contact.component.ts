17be2a0c9b365f364eb350316cae3da9641ce7e2
Ian Wilson <ianwilson1337@gmail.com>
2020-10-23 19:44:29 -0500

    * Formatted the Contact Page * Formatted the Legislation page

--- src/app/Contact/Contact.component.ts
+++ src/app/Contact/Contact.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { officers } from "../Configs/officers";
 
 @Component({
   selector: 'app-contact',
   templateUrl: "./contact.template.html",
   styles: [`
+    .contact-sections-container > * {
+      margin-top: 3rem;
+    }
   `]
 })
-
 export class ContactComponent {
+  public secretary = officers.filter((officer) => officer.position === "Secretary")[0];
   public constructor() {}
 }
--- src/app/Legislation/legislation.component.ts
+++ src/app/Legislation/legislation.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { officers } from "../Configs/officers";
 
 @Component({
   selector: 'app-legislation',
   templateUrl: "./legislation.template.html",
   styles: [`
+    .legislation-section-container > * {
+      margin-top: 2rem;
+    }
   `]
 })
 
 export class LegislationComponent {
+  public trustee = officers.filter((officer) => officer.position === "Trustee")[0];
+  public delegates: string[] = [
+    "Stacy Limas (SADHA President)",
+    "Andrea Wiseman (SADHA Pesident-Elect)",
+    "Mercedes Mendoza (SADHA Trustee)",
+    "Sarah Roddy",
+    "Sandra Mendoza",
+    "Carmela Villareal",
+    "Monna Carpenter-Barin",
+    "Jodie Hostetter",
+    "Saralyn Ferrell",
+    "Grace Uwize-Allen",
+    "Angie Sherlock",
+    "1st Alternate – Celia Aviles",
+    "2ndAlternate – Kelli",
+    "3rd Alternate – Virginia Carrillo"
+  ];
   public constructor() {}
 }
